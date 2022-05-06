03160653b01bc60846ad3170f7c80c55547ee38b
Georgiy Strigun <georgiy.strigun@lanars.com>
2022-05-06 18:31:45 +0300

    Implement SearchBar composable

---
+++ app/src/main/java/com/gsrocks/gsnewsapp/core/presentation/composables/SearchBar.kt
@@ -0,0 +1,101 @@
+package com.gsrocks.gsnewsapp.core.presentation.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gsrocks.gsnewsapp.core.utils.empty
+
+@Composable
+fun SearchBar(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    onSearch: (() -> Unit)? = null
+) {
+    SearchBar(
+        value = TextFieldValue(text = value),
+        onValueChange = { onValueChange(it.text) },
+        modifier = modifier,
+        placeholder = placeholder,
+        onSearch = onSearch
+    )
+}
+
+@Composable
+fun SearchBar(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    onSearch: (() -> Unit)? = null
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(42.dp)
+            .clip(RoundedCornerShape(50))
+            .background(color = MaterialTheme.colors.surface)
+            .padding(horizontal = 8.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = { onSearch?.invoke() }
+            ),
+            singleLine = true,
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Default.Search, contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Box {
+                        if (value.text.isEmpty() && placeholder != null) {
+                            Text(
+                                text = placeholder,
+                                color = Color.Gray
+                            )
+                        }
+                        innerTextField()
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    SearchBar(
+        value = String.empty,
+        onValueChange = {},
+        placeholder = "Search news..."
+    )
+}
